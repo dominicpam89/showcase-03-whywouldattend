6d806858dbf9a4e72bf0295996ecd3f4de609d06
dominicpam89 <dominicpam89@gmail.com>
2024-08-30 21:07:26 +0700

    dry method to add another layout, duh

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,11 +1,20 @@
 import { getEventById } from "@/lib/dummy.data";
 import EventItem from "@/components/event/item";
 import { useRouter } from "next/router";
+import { NextPageWithLayout } from "../_app";
+import { ReactElement } from "react";
+import Layout from "@/components/Layout";
 
-export default function Page() {
+const Page: NextPageWithLayout = () => {
 	const router = useRouter();
 	const eventId = router.query.eventId as string;
 	const event = getEventById(eventId);
 	if (!event) return <p>No event found!</p>;
 	return <EventItem event={event} />;
-}
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+	return <Layout>{page}</Layout>;
+};
+
+export default Page;
